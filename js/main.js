a18259fb8fa9cd72d9b30ca5635173479c835899
christine193 <35804233+christine193@users.noreply.github.com>
2018-10-25 14:03:18 -0400

    anime.js animation

--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
 
-  new WOW().init();
+var hoverArea = document.getElementById('animationArea');
+
+hoverArea.onmouseover = function ()
+{
+    var animate = anime({
+    targets: 'i.fas',
+    loop: false,
+    duration:1000,
+    translateX:[
+      {value:200, duration:500},
+      {value:0, duration:800}
+    ],
+  });
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+new WOW().init();
